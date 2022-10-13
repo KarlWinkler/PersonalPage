98e5463126a45d473bdd3dece063a87678b8e773
karlWinkler <karlw200@gmail.com>
2022-10-12 18:31:42 -0600

    add new styles for home page

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,8 +1,25 @@
 import React from 'react'
+import MagicText from '../Components/MagicText'
+import '../styles/home.scss';
+import '../styles/swipe_up_on_hover.scss';
 
 const Home = () => {
+
+  let setIndex = (index) => {
+    document.querySelector('.home-menu').dataset.index = index
+    console.log('hover')
+  }
+
   return (
-    <div>Home</div>
+    <div className='home'>
+      {/* <MagicText text='Welcome' /> */}
+      <div className='home-menu'>
+        <a className='home-menu-item swipe-up-on-hover' onMouseOver={() => {setIndex(1)}}>About</a>
+        <a className='home-menu-item swipe-up-on-hover' onMouseOver={() => {setIndex(2)}}>Projects</a>
+        <div className='home-menu-background' />
+      </div>
+      <div className='home-background' />
+    </div>
   )
 }
 
