76911ede4c9fc9a9f6dbb66fa23619f89e6345a7
karlWinkler <karlw200@gmail.com>
2022-10-17 17:57:41 -0600

    save users preferred theme

--- src/Components/DarkModeToggle.js
+++ src/Components/DarkModeToggle.js
@@ -1,21 +1,41 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import '../styles/theme-toggle.scss';
 
-let toggleTheme = () => {
-  console.log('click')
-  let mainWrapper = document.querySelector('.app')
-  let themeLabel = document.querySelector('.theme-label')
-
-  mainWrapper.classList.toggle('dark')
-  themeLabel.innerText == 'Light' ? themeLabel.innerText = 'Dark' : themeLabel.innerText = 'Light'
-}
-
 const DarkModeToggle = () => {
+
+  let mode = localStorage.getItem('mode') || 'dark'
+
+  useEffect(() => {
+    setTheme(mode)
+  }, [])
+
+  let toggleTheme = () => {
+    mode = mode === 'dark' ? 'light' : 'dark'
+    setTheme(mode)
+  }
+
+  let setTheme = () => {
+    let mainWrapper = document.querySelector('.app')
+    let themeLabel = document.querySelector('.theme-label')
+
+    if(mode === 'dark') {
+      mainWrapper.classList.add('dark')
+      themeLabel.innerText = 'Dark'
+      localStorage.setItem('mode', 'dark')
+    } else {
+      mainWrapper.classList.remove('dark')
+      themeLabel.innerText = 'Light'
+      localStorage.setItem('mode', 'light')
+    }
+  }
+
+  let checked = localStorage.getItem('mode') === 'dark' ? true : false
+
   return (
     <div className='theme-toggle'>
         <div><label className='theme-label'>Dark</label></div>
       <label className='switch'>
-        <input type='checkbox' defaultChecked={true} className='switch-input' onChange={(e) => {toggleTheme(e)}} />
+        <input type='checkbox' defaultChecked={checked} className='switch-input' onChange={(e) => {toggleTheme(e)}} />
         <span className="slider"></span>
       </label>
     </div>
