1253f051b46a25712e8f74fac72fc82842157911
karlWinkler <karlw200@gmail.com>
2025-01-30 23:57:10 -0700

    remove base from vite config

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,6 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: 'http://karlwinkler.ca/',
   plugins: [vue(), vueDevTools()],
   resolve: {
     alias: {
