d7c2f58e0a0d57233fee71250d3559a1fa05c5cf
karlWinkler <karlw200@gmail.com>
2025-01-30 23:54:56 -0700

    update github workflow to use vite recommended

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: 'http://karlwinkler.ca/dist/',
+  base: 'http://karlwinkler.ca/',
   plugins: [vue(), vueDevTools()],
   resolve: {
     alias: {
